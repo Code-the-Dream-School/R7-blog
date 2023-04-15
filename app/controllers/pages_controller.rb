256102ad8d052953dbc8c614f5218e100a76c17d
gacurl <greg.a.curl@gmail.com>
2023-04-15 16:43:41 -0400

    build form for new blog submission

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,8 @@ class PagesController < ApplicationController
     @page = Page.find(params[:id])
     # Rails only renders one thing per ocntroller action, so remove render and build a view
   end
+
+  def new
+    @page = Page.new
+  end
 end
