6e7395f6718ce15decabb45e0cdeb2ce725128e7
gacurl <greg.a.curl@gmail.com>
2023-04-15 18:23:58 -0400

    improve resource and before_action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
+  before_action :set_page, only: [:show, :edit, :update, :destroy]
+
   def index
     @pages = Page.all
   end
 
   def show
-    @page = Page.find(params[:id])
+    # @page = Page.find(params[:id])
     # Rails only renders one thing per ocntroller action, so remove render and build a view
+    # set_page
   end
 
   def new
@@ -19,17 +22,20 @@ class PagesController < ApplicationController
   end
 
   def edit
-    @page = Page.find(params[:id])
+    # @page = Page.find(params[:id])
+    # set_page
   end
 
   def update
-    @page = Page.find(params[:id])
+    # @page = Page.find(params[:id])
+    # set_page
     @page.update(page_params)
     redirect_to @page
   end
 
   def destroy
-    @page = Page.find(params[:id])
+    # @page = Page.find(params[:id])
+    # set_page
     @page.destroy
     redirect_to pages_path
   end
@@ -38,5 +44,9 @@ class PagesController < ApplicationController
 
     def page_params
       params.require(:page).permit(:title, :body, :slug)
+    end
+
+    def set_page
+      @page = Page.find(params[:id])
     end
 end
