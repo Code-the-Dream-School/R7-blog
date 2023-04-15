f8f901150f99f2419912c720568e200422c06ebe
gacurl <greg.a.curl@gmail.com>
2023-04-15 16:12:19 -0400

    create a show view - each blog post has its own page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,9 @@ class PagesController < ApplicationController
   def index
     @pages = Page.all
   end
+
+  def show
+    @page = Page.find(params[:id])
+    # Rails only renders one thing per ocntroller action, so remove render and build a view
+  end
 end
